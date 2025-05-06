2efdd0bd6ee2b5479e15715351e4abb32d5c98ba
Do Hoan Nguyen <dohoannguyen123@gmail.com>
2025-05-06 17:52:42 +0700

    Update Sensor Code: Read data and write address

--- IOT_Assignment/src/sensor/ES-PH-SOIL-01.h
+++ IOT_Assignment/src/sensor/ES-PH-SOIL-01.h
@@ -4,37 +4,68 @@ struct es_ph_soil_01_data {
 };
 struct es_ph_soil_01 {
     uint8_t slave_address; // Slave address for the sensor
-    uint8_t buffer[8]; 
+    uint8_t tx_buffer[8]; 
+    uint8_t rx_buffer[8];
     es_ph_soil_01_data data; // Data structure to hold pH value
     CRC16 crc; // CRC object for error checking
-    void readSensor() { // Function to read the sensor data
-        buffer[0] = slave_address; // Slave address
-        buffer[1] = 0x03; // Function code for reading holding registers
-        buffer[2] = 0x00; // Starting address high byte
-        buffer[3] = 0x00; // Starting address low byte
-        buffer[4] = 0x00; // Number of registers high byte
-        buffer[5] = 0x01; // Number of registers low byte
+    es_ph_soil_01_data readSensor() { // Function to read the sensor data
+        tx_buffer[0] = slave_address; // Slave address
+        tx_buffer[1] = 0x03; // Function code for reading holding registers
+        tx_buffer[2] = 0x00; // Starting address high byte
+        tx_buffer[3] = 0x00; // Starting address low byte
+        tx_buffer[4] = 0x00; // Number of registers high byte
+        tx_buffer[5] = 0x01; // Number of registers low byte
         // Calculate CRC (Cyclic Redundancy Check) for error checking
-        crc.add((uint8_t *) buffer, 6); // Initial CRC value
+        crc.add((uint8_t *) tx_buffer, 6); // Initial CRC value
         uint16_t crc_result = crc.getCRC(); // Get the CRC value
-        buffer[6] = crc_result >> 8; // CRC high byte
-        buffer[7] = crc_result & 0xFF; // CRC low byte
+        tx_buffer[6] = crc_result >> 8; // CRC high byte
+        tx_buffer[7] = crc_result & 0xFF; // CRC low byte
+        // Send the tx_buffer to the sensor
+        modbus_uart_send(tx_buffer, 8); // Send the data over UART
+        // Receive the response from the sensor
+        size_t bytesRead = modbus_uart_receive(rx_buffer, 7); // Read the response
+        if (bytesRead == 7) { // Check if the response is valid
+            data.PH_value = (rx_buffer[3] << 8) | rx_buffer[4]; // Combine high and low bytes to get the pH value
+            uint16_t crc_received = (rx_buffer[5] << 8) | rx_buffer[6]; // Get the received CRC value
+            crc.add((uint8_t *) rx_buffer, 5); // Calculate CRC for the received data
+            uint16_t crc_calculated = crc.getCRC(); // Get the calculated CRC value
+            if (crc_received != crc_calculated) { // Check if the CRC matches
+                Serial.println("CRC error!"); // Print error message if CRC does not match
+            }
+        } else {
+            Serial.println("Error reading sensor data!"); // Print error message if reading fails
+        }
+        memset(tx_buffer, 0, sizeof(tx_buffer)); // Clear the tx_buffer
+        memset(rx_buffer, 0, sizeof(rx_buffer)); // Clear the rx_buffer
+        return data;
     }
     void setSlaveAddress(uint8_t address) { // Function to set the slave address
         slave_address = address; // Set the slave address
-        buffer[0] = slave_address; // Slave address
-        buffer[1] = 0x10; // Function code for reading holding registers
-        buffer[2] = 0x00; // Starting address high byte
-        buffer[3] = 0x00; // Starting address low byte
-        buffer[4] = 0x00; // Number of registers high byte
-        buffer[5] = 0x01; // Number of registers low byte
+        tx_buffer[0] = slave_address; // Slave address
+        tx_buffer[1] = 0x10; // Function code for reading holding registers
+        tx_buffer[2] = 0x07; // Starting address high byte
+        tx_buffer[3] = 0xD0; // Starting address low byte
+        tx_buffer[4] = 0x00; // Number of registers high byte
+        tx_buffer[5] = 0x01; // Number of registers low byte
         // Calculate CRC (Cyclic Redundancy Check) for error checking
-        crc.add((uint8_t *) buffer, 6); // Initial CRC value
+        crc.add((uint8_t *) tx_buffer, 6); // Initial CRC value
         uint16_t crc_result = crc.getCRC(); // Get the CRC value
-        buffer[6] = crc_result >> 8; // CRC high byte
-        buffer[7] = crc_result & 0xFF; // CRC low byte
-    }
-    int getPH() { // Function to get the pH value
-        return data.PH_value; // Return the pH value
+        tx_buffer[6] = crc_result >> 8; // CRC high byte
+        tx_buffer[7] = crc_result & 0xFF; // CRC low byte
+        modbus_uart_send(tx_buffer, 8); // Send the data over UART
+        // Receive the response from the sensor
+        size_t bytesRead = modbus_uart_receive(rx_buffer, 7); // Read the response
+        if (bytesRead == 7) { // Check if the response is valid
+            uint16_t crc_received = (rx_buffer[5] << 8) | rx_buffer[6]; // Get the received CRC value
+            crc.add((uint8_t *) rx_buffer, 5); // Calculate CRC for the received data
+            uint16_t crc_calculated = crc.getCRC(); // Get the calculated CRC value
+            if (crc_received != crc_calculated) { // Check if the CRC matches
+                Serial.println("CRC error!"); // Print error message if CRC does not match
+            }
+        } else {
+            Serial.println("Error reading sensor data!"); // Print error message if reading fails
+        }
+        memset(tx_buffer, 0, sizeof(tx_buffer)); // Clear the tx_buffer
+        memset(rx_buffer, 0, sizeof(rx_buffer)); // Clear the rx_buffer
     }
 };
