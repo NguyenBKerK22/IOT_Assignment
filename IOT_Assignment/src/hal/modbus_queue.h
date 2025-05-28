e4eb2ca934afac57c8c0309da208dc16e4a9357f
trgnam04 <trungnamtbcm123@gmail.com>
2025-05-28 16:42:45 +0700

    feat(modbus): create a queue for modbus requests

---
+++ IOT_Assignment/src/hal/modbus_queue.h
@@ -0,0 +1,26 @@
+#ifndef MODBUS_QUEUE_H
+#define MODBUS_QUEUE_H
+#pragma once
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/queue.h>
+
+#define MODBUS_QUEUE_LENGTH 10
+
+xQueueHandle xModbusQueue;
+
+typedef struct {
+    uint8_t data;
+    uint16_t time_read;
+    uint8_t timeout;
+    uint8_t* slave_buffer;    
+    TaskHandle_t task_handle;
+} ModbusRequest_t;
+
+void initModbusQueue(void);
+void sendModbusQueue(ModbusRequest_t request, uint8_t priority);
+
+
+
+
+#endif // MODBUS_QUEUE_H
